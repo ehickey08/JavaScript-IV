9d41f4e66f393f7af8202c588befaaf78995cd7b
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-05-09 15:11:32 -0400

    Test each class with logs and methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,11 +1,11 @@
 // CODE here for your Lambda Classes
 //You'll need to create 2 - 3 objects for each class and test them according to their unique Attributes
 class Person {
-    constructor (props) {
-        this.name = props.name;
-        this.age = props.age;
-        this.location = props.location;
-        this.gender = props.gender
+    constructor ({name, age, location, gender}) {
+        this.name =name;
+        this.age = age;
+        this.location = location;
+        this.gender = gender
     }
 
     speak () {
@@ -14,11 +14,11 @@ class Person {
 }
 
 class Instructor extends Person {
-    constructor (props){
-        super(props);
-        this.specialty = props.specialty;
-        this.favLanguage = props.favLanguage;
-        this.catchPhrase = props.catchPhrase;
+    constructor ({specialty, favLanguage, catchPhrase, ...rest}){
+        super(rest);
+        this.specialty = specialty;
+        this.favLanguage = favLanguage;
+        this.catchPhrase = catchPhrase;
     }
 
     demo (subject) {
@@ -30,12 +30,12 @@ class Instructor extends Person {
     }
 }
 
-class Students extends Person {
-    constructor (props) {
-        super(props);
-        this.previousBackground = props.previousBackground;
-        this.className = props.className;
-        this.favSubjects = props.favSubjects;
+class Student extends Person {
+    constructor ({previousBackground, className, favSubjects, ...rest}) {
+        super(rest);
+        this.previousBackground = previousBackground;
+        this.className = className;
+        this.favSubjects = favSubjects;
     }
 
     listsSubjects() {
@@ -47,15 +47,15 @@ class Students extends Person {
     }
 
     sprintChallenge (subject) {
-        console.log(`${this.name} has begun sprint challnege on ${subject}.`)
+        console.log(`${this.name} has begun sprint challenge on ${subject}.`)
     }
 }
 
 class ProjectManager extends Instructor {
-    constructor(props){
-        super(props);
-        this.gradClassName = props.gradClassName;
-        this.favInstructor = props.favInstructor;
+    constructor({gradClassName, favInstructor, ...rest}){
+        super(rest);
+        this.gradClassName = gradClassName;
+        this.favInstructor = favInstructor;
     }
 
     standUp (channel) {
@@ -66,3 +66,125 @@ class ProjectManager extends Instructor {
         console.log(`${this.name} debugs ${student.name}'s code on ${subject}.`)
     }
 } 
+
+//Create random Persons
+const bill = new Person ({
+    name: 'Bill',
+    age: 30,
+    location: 'Liverpool',
+    gender: 'Male'
+})
+
+const jill = new Person ({
+    name: 'Jill',
+    age: 57,
+    location: 'Barcelona',
+    gender: 'Female'
+})
+
+const steph = new Person ({
+    name: 'Steph',
+    age: 28,
+    location: 'Oakland',
+    gender: 'Male'
+})
+
+//Create random Instructors
+
+const dan = new Instructor({
+    name: 'Dan',
+    age: 42,
+    location: 'Denver',
+    gender: 'Male',
+    specialty: 'node.js',
+    favLanguage: 'JavaScript',
+    catchPhrase: 'I love cats!'
+})
+
+const rick = new Instructor ({
+    name: 'Rick',
+    age: 17,
+    location: 'Boston',
+    gender: 'Male',
+    specialty: 'redux',
+    favLanguage: 'Python',
+    catchPhrase: 'What am I doing here>'
+})
+
+//Create random Students
+
+const ethan = new Student ({
+    name: 'Ethan',
+    age: 29,
+    location: 'Orlando',
+    gender: 'Male',
+    previousBackground: 'Teacher',
+    className: 'Web20',
+    favSubjects: ['Html', ' CSS', ' JavaScript']
+})
+
+const joe = new Student ({
+    name: 'Joe',
+    age: 71,
+    location: 'Augusta',
+    gender: 'Male',
+    previousBackground: 'NFL Quarterback',
+    className: 'Web18',
+    favSubjects: ['React' , ' Python', ' AJAX']
+})
+
+//Create random PMs
+
+const josh = new ProjectManager({
+    name: 'Josh',
+    age: 32,
+    location: 'Austin',
+    gender: 'Male',
+    specialty: 'node.js',
+    favLanguage: 'JavaScript',
+    catchPhrase: 'I love dogs!',
+    gradClassName: 'Web19',
+    favInstructor: 'Sean'
+})
+
+const jeremiah = new ProjectManager({
+    name: 'Jeremiah',
+    age: 35,
+    location: 'Utah',
+    gender: 'Male',
+    specialty: 'PHP',
+    favLanguage: 'Ruby',
+    catchPhrase: 'Good job man!',
+    gradClassName: 'Web5',
+    favInstructor: 'Austen'
+})
+//Test Persons
+
+console.log(`\n*****Persons*****`);
+console.log(bill, jill, steph);
+bill.speak();
+jill.speak();
+steph.speak();
+
+//Test Instructors
+
+console.log(`\n*****Instructors*****`);
+console.log(dan, rick);
+dan.demo('Advanced CSS');
+dan.speak();
+rick.grade(ethan, 'Javascript IV');
+
+//Test Students
+
+console.log(`\n****Students*****`);
+console.log(ethan, joe);
+ethan.listsSubjects();
+joe.PRAssignment();
+ethan.sprintChallenge('JavaScript');
+
+//Test Project Managers
+
+console.log(`\n****Project Managers*****`);
+console.log(josh, jeremiah);
+josh.standUp('web20_help');
+jeremiah.debugsCode(joe, 'CSS');
